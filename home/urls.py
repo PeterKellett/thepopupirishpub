afe6d0568fce6ef9f83dddde3a22df3548f941e8
PeterKellett <52170335+PeterKellett@users.noreply.github.com>
2026-09-09 13:34:12 +0100

    Changed images to webp

--- home/urls.py
+++ home/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('aboutus', views.aboutus, name='aboutus'),
-    path('kiwobar', views.kiwobar, name='kiwobar'),
-    path('guinnessbar', views.guinnessbar, name='guinnessbar'),
-    path('mobilebar', views.mobilebar, name='mobilebar'),
-    path('custombar', views.custombar, name='custombar'),
+    path('kiwobar/', views.kiwobar, name='kiwobar'),
+    path('guinnessbar/', views.guinnessbar, name='guinnessbar'),
+    path('mobilebar/', views.mobilebar, name='mobilebar'),
+    path('custombar/', views.custombar, name='custombar'),
     # path("contact/", ContactView.as_view(), name="contact"),
     # path("success/", SuccessView.as_view(), name="success"),
 ]
